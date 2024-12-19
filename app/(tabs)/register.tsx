356f33ac805279824cf08277ec625e599affda79
woozai <wooozai@gmail.com>
2024-12-19 18:12:36 +0200

    updated packages

--- app/(tabs)/register.tsx
+++ app/(tabs)/register.tsx
@@ -1,42 +1,30 @@
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { Text, TextInput, View, Button, TouchableOpacity } from "react-native";
+import tw from "tailwind-react-native-classnames";
 
 export default function RegisterScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>הרשמה</Text>
-      <TextInput style={styles.input} placeholder="שם מלא" />
-      <TextInput style={styles.input} placeholder="אימייל" keyboardType="email-address" />
-      <TextInput style={styles.input} placeholder="סיסמה" secureTextEntry />
-      <Button title="הירשם" onPress={() => alert('נרשמת בהצלחה!')} />
-      <Text style={styles.link}>כבר יש לך חשבון? עבור לדף התחברות</Text>
+    <View style={tw`flex-1 items-center justify-center bg-white px-5`}>
+      <Text style={tw`text-2xl font-bold mb-5`}>הרשמה</Text>
+      <TextInput
+        style={tw`w-full p-3 border border-gray-300 rounded mb-3`}
+        placeholder="שם מלא"
+      />
+      <TextInput
+        style={tw`w-full p-3 border border-gray-300 rounded mb-3`}
+        placeholder="hello"
+        keyboardType="email-address"
+      />
+      <TextInput
+        style={tw`w-full p-3 border border-gray-300 rounded mb-3`}
+        placeholder="סיסמה"
+        secureTextEntry
+      />
+      <Button title="הירשם" onPress={() => alert("נרשמת בהצלחה!")} />
+      <TouchableOpacity>
+        <Text style={tw`mt-5 text-blue-500 underline`}>
+          כבר יש לך חשבון? עבור לדף התחברות
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
-    padding: 20,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  input: {
-    width: '100%',
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    marginBottom: 10,
-    borderRadius: 5,
-  },
-  link: {
-    marginTop: 20,
-    color: 'blue',
-    textDecorationLine: 'underline',
-  },
-});
