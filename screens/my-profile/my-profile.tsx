0a0467d95f6b73987f5dececaa99bcbe1f2809ec
woozai <wooozai@gmail.com>
2025-01-07 22:56:02 +0200

    redo

--- screens/my-profile/my-profile.tsx
+++ screens/my-profile/my-profile.tsx
@@ -7,10 +7,7 @@ import {
   ScrollView,
   TouchableOpacity,
   ActivityIndicator,
-  SafeAreaView,
-  StatusBar,
 } from "react-native";
-import { useNavigation } from "@react-navigation/native";
 import { MongoUser } from "../../interfaces/user-interface";
 import { useAuth } from "../../contexts/auth-context";
 import { useFocusEffect } from "@react-navigation/native";
@@ -19,7 +16,6 @@ const ProfilePage = () => {
   const [user, setUser] = useState<MongoUser | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const { mongoId } = useAuth();
-  const navigation = useNavigation();
 
   useFocusEffect(
     useCallback(() => {
@@ -67,68 +63,49 @@ const ProfilePage = () => {
   }
 
   return (
-    <SafeAreaView className="flex-1 bg-white">
-      {/* StatusBar Configuration */}
-      <StatusBar barStyle="dark-content" backgroundColor="white" />
-
+    <View className="flex-1 bg-white">
       {/* Header */}
-      <View className="flex-row items-center justify-between px-4 py-3 border-b border-gray-200 bg-white">
-        <Text className="text-xl font-bold"></Text>
-        <TouchableOpacity
-          onPress={() => navigation.openDrawer()} // Replace with your settings screen route
-        >
+      <View className="flex-row justify-between items-center p-5 bg-gray-100"></View>
+
+      {/* Scrollable Content */}
+      <ScrollView className="flex-1 p-4">
+        {/* Profile Info */}
+        <View className="flex-row items-center mb-4">
           <Image
             source={{
-              uri: "https://cdn-icons-png.flaticon.com/512/2099/2099058.png",
+              uri: "https://cdn-icons-png.flaticon.com/512/147/147144.png",
             }}
-            className="w-6 h-6"
+            className="w-20 h-20 rounded-full mr-4"
           />
-        </TouchableOpacity>
-      </View>
-
-      {/* Main Content */}
-      <View className="flex-1">
-        <ScrollView className="flex-1 p-4">
-          {/* Profile Info */}
-          <View className="flex-row items-center mb-4">
-            <Image
-              source={{
-                uri: "https://cdn-icons-png.flaticon.com/512/147/147144.png",
-              }}
-              className="w-20 h-20 rounded-full mr-4"
-            />
-            <View className="flex-1">
-              <Text className="text-lg font-bold">{user.username}</Text>
-              <Text className="text-lg font-bold mb-1">{`${user.first_name} ${user.last_name}`}</Text>
-              <Text className="text-sm text-gray-500 mb-2">
-                Rank: Adventurer
-              </Text>
-              <TouchableOpacity className="bg-gray-200 px-4 py-2 rounded mb-2 flex-row items-center">
-                <Image
-                  source={{
-                    uri: "https://cdn-icons-png.flaticon.com/512/847/847969.png",
-                  }}
-                  className="w-4 h-4 mr-2"
-                />
-                <Text>Hikers</Text>
-              </TouchableOpacity>
-            </View>
+          <View className="flex-1">
+            <Text className="text-lg font-bold">{user.username}</Text>
+            <Text className="text-lg font-bold mb-1">{`${user.first_name} ${user.last_name}`}</Text>
+            <Text className="text-sm text-gray-500 mb-2">Rank: Adventurer</Text>
+            <TouchableOpacity className="bg-gray-200 px-4 py-2 rounded mb-2 flex-row items-center">
+              <Image
+                source={{
+                  uri: "https://cdn-icons-png.flaticon.com/512/847/847969.png",
+                }}
+                className="w-4 h-4 mr-2"
+              />
+              <Text>Hikers</Text>
+            </TouchableOpacity>
           </View>
+        </View>
 
-          {/* Bio */}
-          <View className="mt-4">
-            <Text className="text-sm font-bold mb-2">Bio</Text>
-            <TextInput
-              className="border border-gray-300 p-2 rounded min-h-[60px]"
-              placeholder="Write your bio here..."
-              editable={false}
-              multiline
-              value={user.bio}
-            />
-          </View>
-        </ScrollView>
-      </View>
-    </SafeAreaView>
+        {/* Bio */}
+        <View className="mt-4">
+          <Text className="text-sm font-bold mb-2">Bio</Text>
+          <TextInput
+            className="border border-gray-300 p-2 rounded min-h-[60px]"
+            placeholder="Write your bio here..."
+            editable={false}
+            multiline
+            value={user.bio}
+          />
+        </View>
+      </ScrollView>
+    </View>
   );
 };
 
