ef1fcbb2d4464595c713c1ba23ca61f208012ad4
woozai <wooozai@gmail.com>
2025-01-13 00:25:30 +0200

    nothing damn

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,13 @@
 module.exports = function (api) {
   api.cache(true);
-  const ENV = process.env.APP_ENV || "prod"; // Default to local environment
+  const ENV = process.env.APP_ENV || "local"; // Default to prod
+  console.log(`Using environment: ${ENV}`);
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      "nativewind/babel", // Include nativewind
+      "nativewind/babel",
       [
-        "module:react-native-dotenv", // Include react-native-dotenv
+        "module:react-native-dotenv",
         {
           moduleName: "@env",
           path: `.env.${ENV}`,
