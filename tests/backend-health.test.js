fd93798d89e2cf764e56dcf23215a830ecfa0f12
woozai <wooozai@gmail.com>
2025-04-12 17:41:21 +0300

    console url

--- tests/backend-health.test.js
+++ tests/backend-health.test.js
@@ -1,6 +1,7 @@
 describe("Backend Health Check", () => {
   it("should respond with 'Server is working'", async () => {
     const baseUrl = process.env.EXPO_LOCAL_SERVER || "http://localhost:3000";
+    console.log("baseUrl", baseUrl);
     const response = await fetch(`${baseUrl}/api`);
     const data = await response.text();
 
